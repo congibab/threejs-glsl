afac02767a5e3c85adf6d3e24b5b167e3533ff75
congibab <engljapen2@gmail.com>
2021-08-16 02:06:17 +0900

    Fresnel Effect fuction create

--- src/glsl/test.frag
+++ src/glsl/test.frag
@@ -3,8 +3,10 @@ precision mediump int;
 
 varying vec2 vUv;
 varying vec3 vNormal;
-
 varying vec3 ViewDir;
+
+uniform sampler2D texture1;
+uniform float time;
 
 float Fresnel(vec3 Normal, vec3 ViewDir, float Power)
 { 
@@ -13,11 +15,10 @@ float Fresnel(vec3 Normal, vec3 ViewDir, float Power)
     float test1 = clamp(test2, 0.0, 1.0);
 
     return pow(1.0 - test1 , Power);
-    //return test2;
 }
 
 void main()
 {
     float _FresnelEffect = Fresnel(vNormal, ViewDir, 2.0);
-    gl_FragColor=vec4(1.0 ,0.0 , 0.0, 1.0) * _FresnelEffect;
+    gl_FragColor= vec4(0.0 , 1.0 , 1.0, _FresnelEffect) * texture2D(texture1, fract(vUv * 2.0));
 }
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import vShader from './glsl/test.vert';
 import fShader from './glsl/test.frag';
 
+let Time : number;
+let deltaTime : number;
+
 window.addEventListener("DOMContentLoaded", () => {
+
+  const clock = new THREE.Clock();
+  Time = clock.getElapsedTime();
+  deltaTime = clock.getDelta();
 
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -19,17 +26,20 @@ window.addEventListener("DOMContentLoaded", () => {
 
   console.log(vShader);
 
+  const texture = new THREE.TextureLoader().load("/dist/asset/textures/HexPulse.png");
   const geometry = new THREE.SphereGeometry();
-  const material = new THREE.ShaderMaterial({
+  
+  const uniforms = {
+    "texture1" : { value : texture},
+    "time": { value: Time },
+  }
 
-    uniforms: {
-      "time": { value: 1.0 },
-    },
+  const material = new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    transparent : true,
     vertexShader: vShader,
     fragmentShader: fShader
   });
-
-
   const shpere = new THREE.Mesh(geometry, material);
   scene.add(shpere);
   shpere.position.y = 1.0;
@@ -47,6 +57,10 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function animate() {
     requestAnimationFrame(animate);
+
+    Time = clock.getElapsedTime();
+    deltaTime = clock.getDelta();
+    uniforms['time'].value = Time;
 
     controls.update();
     renderer.render(scene, camera);
